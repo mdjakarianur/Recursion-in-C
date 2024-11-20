a1bfa900419a67cfcac860264f921b8162fcca40
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-20 18:19:32 +0600

    Create 01 Factorial finding by Recursion.c

---
+++ 01 Factorial finding by Recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int recursiveFactorial();
+int main(){
+    int x,n=5;
+    x=recursiveFactorial(n);
+    printf("%d",x);
+    return 0;
+}
+
+int recursiveFactorial(int n){
+    if (n==0){
+        return 1;
+    }
+    else{
+        return (n*(recursiveFactorial(n-1)));
+    }
+}
